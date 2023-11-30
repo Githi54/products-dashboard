17277a193e17a803da9ed757fd957b47271c6929
Mikita Havryliuk <chinter.games1@gmail.com>
2023-11-30 15:29:20 +0200

    feature: install some libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return (<h1>Start work</h1>)
+  return (
+    <h1 className="
+      text-3xl text-blue font-medium 
+      underline cursor-pointer
+      "
+    >
+      Start work
+    </h1>
+  )
 }
 
 export default App
