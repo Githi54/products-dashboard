f5e129f83fd66969e7d19daae4c516e01ed41681
Mikita Havryliuk <chinter.games1@gmail.com>
2023-12-01 13:37:33 +0200

    fixes: some test fixes

--- src/tests/unit/data.test.js
+++ src/tests/unit/data.test.js
@@ -2,10 +2,6 @@ import { describe, test, expect } from "vitest";
 import { seedProductList } from '@data'
 
 describe('Seeding data test', () => {
-    test('Should error if size bigger than products', () => {
-        expect(() => seedProductList(1000)).toThrowError()
-    })
-
     test('Should only unique names in products', () => {
         const productList = seedProductList(7)
         const uniqueProductNames = [...new Set(productList.map(({ name }) => name))]
@@ -16,5 +12,9 @@ describe('Seeding data test', () => {
     test('Should result length equal size', () => {
         const randomSize = Math.floor(Math.random() * 10)
         expect(seedProductList(randomSize).length).toEqual(randomSize)
+    })
+
+    test('Should work without arguments', () => {
+        expect(seedProductList().length).toEqual(10)
     })
 })
